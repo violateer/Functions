a5153eec39e2f71918509af8895e477fbd7d1870
violateer <1828257089@qq.com>
2020-10-15 22:52:14 +0800

    创建Promise基本框架

--- 手写Promise/promise.js
+++ 手写Promise/promise.js
@@ -1,3 +1,30 @@
-const test = () => {
-    console.log('test');
+function Promise(excutor) {
+    // 初始化对象
+    let self = this
+    self.status = 'pending'
+    self.value = null
+    self.reason = null
+
+    // 创建resolve函数
+    function resolve(value) {
+        if (self.status === 'pending') {
+            self.value = value
+            self.status = 'fulfilled'
+        }
+    }
+
+    // 创建reject函数
+    function reject(reason) {
+        if (self.status === 'pending') {
+            self.reason = reason
+            self.status = 'rejected'
+        }
+    }
+
+    // 捕捉错误调用reject
+    try {
+        excutor(resolve, reject)
+    } catch (err) {
+        reject(err)
+    }
 }
