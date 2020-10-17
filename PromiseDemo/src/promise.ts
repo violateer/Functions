65d0bc08445cd9f6580748a95a49fa639cfffa50
violateer <1828257089@qq.com>
2020-10-17 22:06:08 +0800

    搭建then方法和函数立即执行

---
+++ PromiseDemo/src/promise.ts
@@ -0,0 +1,11 @@
+class myPromise {
+    constructor(fn) {
+        if (typeof fn !== "function") {
+            throw new Error("我只接收函数")
+        }
+        fn()
+    }
+    then() {}
+}
+
+export default myPromise
--- PromiseDemo/test/index.ts
+++ PromiseDemo/test/index.ts
@@ -1,10 +1,37 @@
 import * as chai from "chai"
+import Promise from "../src/promise"
 
 const assert = chai.assert
 
-describe("Chai的使用", () => {
-    it("测试相等", () => {
+describe("Promise", () => {
+    it("是一个类", () => {
+        assert.isFunction(Promise)
+        assert.isObject(Promise.prototype)
+    })
+    it("new Promise如果接收的不是一个函数就报错", () => {
+        assert.throw(() => {
+            //@ts-ignore
+            new Promise()
+        })
+        assert.throw(() => {
+            //@ts-ignore
+            new Promise(1)
+        })
+        assert.throw(() => {
+            //@ts-ignore
+            new Promise(false)
+        })
+    })
+    it("new Promise(fn)生成一个对象， 对象有then方法", () => {
+        const promise = new Promise(() => { })
+        assert.isFunction(promise.then)
+    })
+    it("new Promise(fn)应是立即执行", () => {
+        let called = false
+        const promise = new Promise(() => {
+            called = true
+        })
         // @ts-ignore
-        assert(1===2)
+        assert(called === true)
     })
 })
