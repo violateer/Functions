7234f1e61995a3a36789b34efc49ba846f55be01
violateer <1828257089@qq.com>
2021-01-18 18:51:36 +0800

    实现off

--- EventHubDemo/src/index.ts
+++ EventHubDemo/src/index.ts
@@ -5,17 +5,32 @@ class EventHub {
     //  '羊城晚报':[fn1,fn2]
     //}
     on(eventName, fn) {
-        // 初始化
-        this.cache[eventName] = this.cache[eventName] || [];
         // 将fn存入 this.cache[eventName]
+        this.cache[eventName] = this.cache[eventName] || [];
         this.cache[eventName].push(fn);
     }
     
-    emit(eventName, data) {
-        // 初始化
-        this.cache[eventName] = this.cache[eventName] || [];
+    emit(eventName, data?) {
         // 将 this.cache[eventName]里面的fn依次调用
+        this.cache[eventName] = this.cache[eventName] || [];
         this.cache[eventName].forEach(fn => fn(data));
+    }
+    
+    off(eventName, fn) {
+        // 将fn从 this.cache[eventName]删除
+        this.cache[eventName] = this.cache[eventName] || [];
+        let index;
+        for (let i = 0; i < this.cache[eventName].length; i++) {
+            if (this.cache[eventName][i] === fn) {
+                index = i;
+                break;
+            }
+        }
+        if (index === undefined) {
+            return;
+        } else {
+            this.cache[eventName].splice(index, 1);
+        }
     }
 }
 
--- EventHubDemo/test/index.ts
+++ EventHubDemo/test/index.ts
@@ -12,3 +12,15 @@ eventHub.on('xxx', (data) => {
     console.assert(data === '买水喝');
 });
 eventHub.emit('xxx', '买水喝');
+
+const eventHub2 = new EventHub();
+let called2 = false;
+const fn1 = () => {
+    called2 = true;
+};
+eventHub.on('yyy', fn1);
+eventHub.off('yyy', fn1);
+eventHub.emit('yyy');
+setTimeout(() => {
+    console.log('called2：' + called2);
+}, 1000);
