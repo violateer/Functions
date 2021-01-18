74142d1b20b6c87d3f324f3522adb0ad012aa118
violateer <1828257089@qq.com>
2021-01-18 18:59:12 +0800

    优化逻辑代码

--- EventHubDemo/src/index.ts
+++ EventHubDemo/src/index.ts
@@ -1,5 +1,5 @@
 class EventHub {
-    cache = {};
+    private cache = {};
     //{
     //  '楚天都市报':[fn1,fn2,fn3],
     //  '羊城晚报':[fn1,fn2]
@@ -18,20 +18,22 @@ class EventHub {
     
     off(eventName, fn) {
         // 将fn从 this.cache[eventName]删除
-        this.cache[eventName] = this.cache[eventName] || [];
-        let index;
-        for (let i = 0; i < this.cache[eventName].length; i++) {
-            if (this.cache[eventName][i] === fn) {
-                index = i;
-                break;
-            }
-        }
-        if (index === undefined) {
-            return;
-        } else {
-            this.cache[eventName].splice(index, 1);
-        }
+        const index = indexOf(this.cache[eventName], fn);
+        if (index === -1) return;
+        this.cache[eventName].splice(index, 1);
     }
 }
 
 export default EventHub;
+
+function indexOf(array, item) {
+    if (array === undefined) return -1;
+    let index = -1;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
